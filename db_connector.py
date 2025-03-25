705c6925c545a1fc34586dca52a0ff1e2e64e86d
bths <@bths.edu>
2025-03-25 13:04:46 -0400

    programming database

--- db_connector.py
+++ db_connector.py
@@ -6,15 +6,21 @@ from mysql.connector import errorcode
 load_dotenv()
 
 user = os.environ.get('DB_USERNAME')
-password = os.environ.get('DB_USERNAME')
+password = os.environ.get('DB_PASSWORD')
 
 try:
     connection = mysql.connector.connect(
         user=user, 
         password=password, 
-        host='10.8.37.99', 
-        databbase='not existing'
+        host='10.8.37.226', 
+        database='andrewz47_db'
     )
+
+    cursor = connection.cursor()
+    cursor.execute("show tables;")
+
+    print(cursor.fetchall())
+    
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("Something is wrong with your user name or password")

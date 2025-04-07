bfa2d738522abf7c47f2ddf770e97bf0a36f49a8
BT_4N2_31 <@bths.edu>
2025-04-07 13:18:37 -0400

    it kinda works... dying myself...

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from dotenv import find_dotenv, load_dotenv
 from datagenerator import DataGenerator
 from dbconnection import DBConnection
 
+import paramiko
+import paramiko.util
+from base64 import decodebytes
+
 
 def check_queries(seed=None):
     if not find_dotenv():
@@ -15,9 +19,11 @@ def check_queries(seed=None):
     sql_runner = 'run_sql.sh'
 
     load_dotenv()
-    user = os.environ.get('DB_USERNAME')
-    password = os.environ.get('DB_PASSWORD')
-    name = os.environ.get('DB_NAME')
+    db_user = os.environ.get('DB_USERNAME')
+    db_password = os.environ.get('DB_PASSWORD')
+    db_name = os.environ.get('DB_NAME')
+    linux_user = os.environ.get('LINUX_USERNAME')
+    linux_password = os.environ.get('LINUX_PASSWORD')
 
     if not os.path.isdir(output_dir):
         os.mkdir(output_dir)
@@ -37,12 +43,27 @@ def check_queries(seed=None):
         ('grades_output.sql', dg.generate_grades())
     ]
 
-    with open(sql_runner, 'w') as s:
+    with open(sql_runner, 'w', newline='\n') as s:
+        s.write("#!/bin/bash\n")
+
         for filename, queries in sql_files:
             with open(os.path.join(output_dir, filename), 'w') as f:
                 for query in queries:
                     f.write(query + '\n')
-            s.write(f"mysql -u {user} -p {password} -h 10.8.37.226 -D {name} < output/{filename}")
+            s.write(f"mysql -u{db_user} -p{db_password} -h 10.8.37.226 -D {db_name} < output/{filename}\n")
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect('10.8.37.99', username=linux_user, password=linux_password)
+
+    print(f'/home/{linux_user}/Projects/Database_Design_Project')
+    sftp = ssh.open_sftp()
+    sftp.put('./run_sql.sh', f'/home/{linux_user}/Projects/Database_Design_Project/run_sql.sh')
+
+    sftp.mkdir(f'/home/{linux_user}/Projects/Database_Design_Project/output')
+    for filename, _ in sql_files:
+        print(os.path.join(output_dir, filename))
+        sftp.put(os.path.join(output_dir, filename), f'/home/{linux_user}/Projects/Database_Design_Project/output/{filename}')
 
 
 
@@ -72,4 +93,5 @@ def main():
 
 
 if __name__ == "__main__":
+    paramiko.util.log_to_file("paramiko.log")
     main()
